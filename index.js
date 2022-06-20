85c49b826d07ed94bab6dfc5dae0de9411415b57
Terormumuli <terahmumuli@gmail.com>
2022-06-20 21:02:21 +0300

    DOM Modifying

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const first = document.querySelector('main')
+first.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Alvin is the champion" 
